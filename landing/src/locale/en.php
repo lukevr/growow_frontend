1bdab533dff7b2bcf57e7ae734773bc18d8f8c2b
Rett Pop <rettpop@leftbit.com>
2016-07-14 15:12:50 +0300

    New language

---
+++ landing/src/locale/en.php
@@ -0,0 +1,19 @@
+<?php
+
+    $words = array(
+
+    "FQ1" => "What I will get?",
+    "FA1" => "Farmer will get subscribers, who will pay for the farm service on the farm’s fields. So, the short answer is additional income.",
+
+    "FQ2" => "What do I need to participate?",
+    "FA21" => "You should have a piece of soil which can be layouted and prepared to be planted by vegetables. Also you have to have set of GroWOW hardware equipment that will provide monitoring and control possibility to the subscriber (user) as well.",
+
+
+    "meettheteam.Title" => "Meet the team",
+    "persons.Victor.Name" => "Victor Radchenko",
+    "persons.Victor.Position" => "CEO & Founder",
+    "persons.Slava.Name" => "Vyacheslav Zhelobkov",
+    "persons.Slava.Position" => "CTO & Cofounder",
+);
+
+?>
